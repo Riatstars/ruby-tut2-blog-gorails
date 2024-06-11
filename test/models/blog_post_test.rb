3d1761cf6a8aba6d0770d1dcebc1e31ed70a794b
“nampnt” <nampnt@smartosc.com>
2024-06-11 10:59:30 +0700

    add sorting nulls last and testing

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,41 @@
-require "test_helper"
+require 'test_helper'
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'draft? return true for draft blog post' do
+    blog_post = blog_posts(:draft)
+    assert blog_post.draft?
+  end
+
+  test 'draft? return false for published blog post' do
+    blog_post = blog_posts(:published)
+    refute blog_post.draft?
+  end
+  test 'draft? return false for schedulted blog post' do
+    blog_post = blog_posts(:scheduled) 
+    refute blog_post.draft?
+  end
+  test 'published? return true for published blog post' do
+    blog_post = blog_posts(:published)
+    assert blog_post.published?
+  end
+  test 'published? return false for draft blog post' do
+    blog_post = blog_posts(:draft)
+    refute blog_post.published?
+  end
+  test 'published? return false for scheduled blog post' do
+    blog_post = blog_posts(:scheduled) 
+    refute blog_post.published?
+  end
+  test 'schedulted? return true for scheduled blog post' do
+    blog_post = blog_posts(:scheduled) 
+    assert blog_post.scheduled?
+  end
+  test 'schedulted? return false for draft blog post' do
+    blog_post = blog_posts(:draft)
+    refute blog_post.scheduled?
+  end
+  test 'schedulted? return false for published blog post' do
+    blog_post = blog_posts(:published)
+    refute blog_post.scheduled?
+  end
 end
