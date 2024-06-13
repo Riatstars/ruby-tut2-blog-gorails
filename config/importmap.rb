631474c45f359a60329039931eeef259cc0309cd
“nampnt” <nampnt@smartosc.com>
2024-06-13 17:51:21 +0700

    add fontawesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@flaticon/flaticon-uicons", to: "../node_modules/@flaticon/flaticon-uicons/css/all/all.css", preload: true
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.2/js/all.js" # @6.5.2
